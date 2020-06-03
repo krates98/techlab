b4b8c45c55f14859a8d18045dab87c873ef25b1f
Your Name <krates198@gmail.com>
2020-06-03 22:06:57 +0530

    fixed some files

--- routes/offers.js
+++ routes/offers.js
@@ -1,14 +1,27 @@
 const   express         = require("express"),
         Offer           = require("../models/offers"),
+        Counter         = require("../models/counter"),
         router          = express.Router();
     
     // Fetch Offer Pages
 
     router.get("/pages", isLoggedIn,async function(req,res){
+
+        var counte = await Counter.find(function( err,result){
+            return result;
+        })
+        var cx = {count: (counte[0].count+1)};
+        Counter.findOneAndUpdate(counte,cx,function(err,upd){
+            if(err){
+                console.log("failed");
+            } else {
+                console.log("updated")
+            }
+        })  
         var offurl = await Offer.find({toggle: true}, function(err,result){
             return result;
         });
-        res.render("pages/pages",{offurl});
+        res.render("pages/pages",{offurl,counte});
     });
 
     // middleware
